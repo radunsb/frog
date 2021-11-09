38bd9951285be8d53a6e6a50c32ed116c1a2766d
radunsb <93169946+radunsb@users.noreply.github.com>
2021-11-09 12:12:50 -0500

    Add files via upload

---
+++ src/Player.java
@@ -0,0 +1,8 @@
+public class Player extends Unit{
+    int playerX;
+    int playerY;
+    public Player(int playerX, int playerY){
+        super(playerX);
+        this.playerY = playerY;
+    }
+}
