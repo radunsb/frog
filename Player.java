605335dad156c112a469930e89c06e997cdeca12
radunsb <93169946+radunsb@users.noreply.github.com>
2021-11-09 12:17:01 -0500

    Delete Player.java

--- Player.java
+++
@@ -1,8 +0,0 @@
-public class Player extends Unit{
-    int playerX;
-    int playerY;
-    public Player(int playerX, int playerY){
-        super(playerX);
-        this.playerY = playerY;
-    }
-}
